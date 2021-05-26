1605e04bb07c1aa8a75ab7e9aedfbad50e5f132a
mandreage <217030187@uppenjamo.edu.mx>
2021-05-26 10:17:34 -0500

    pantallas login y packing

---
+++ app/src/main/java/com/example/pokayokeeaton/Packing.java
@@ -0,0 +1,14 @@
+package com.example.pokayokeeaton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Packing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_packing);
+    }
+}
