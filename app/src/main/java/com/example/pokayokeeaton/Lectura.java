e2362c454365ae609baf8a2cf78308209ba22942
Luis Arellano <luis_bravo_99@hotmail.com>
2021-05-28 17:03:01 -0500

    Creador de archivo y clase de encriptación

--- app/src/main/java/com/example/pokayokeeaton/Lectura.java
+++ app/src/main/java/com/example/pokayokeeaton/Lectura.java
@@ -1,24 +1,54 @@
 package com.example.pokayokeeaton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.pokayokeeaton.Modelos.RSA;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Lectura extends AppCompatActivity {
     ArrayList<String> DatosRec;
     EditText edCodigo2D, edAIAG, edLineSet;
     TextView tvCantidad;
     LinearLayout layoutSetLine;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lectura);
+        int PERMISSION = ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(PERMISSION != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
+        }
         Bundle extras = getIntent().getExtras();
         DatosRec = extras.getStringArrayList("datos");
         edCodigo2D = findViewById(R.id.edCodigo2D);
@@ -26,12 +56,13 @@ public class Lectura extends AppCompatActivity {
         edLineSet = findViewById(R.id.edLineSet);
         tvCantidad = findViewById(R.id.tvCantidad);
         layoutSetLine = findViewById(R.id.LayoutLineSetPrincipal);
+        crearArchivo();
         if(IsTwo(DatosRec)){
-            tvCantidad.setText(" 0 / 2");
+            tvCantidad.setText(" 0 / " + DatosRec.get(2));
             layoutSetLine.setVisibility(View.INVISIBLE);
         }
         else{
-            tvCantidad.setText(" 0 / 3 ");
+            tvCantidad.setText(" 0 /  " + DatosRec.get(2));
             layoutSetLine.setVisibility(View.VISIBLE);
         }
     }
@@ -49,4 +80,65 @@ public class Lectura extends AppCompatActivity {
         }
         return estado;
     }
+
+    private void crearArchivo(){
+        try{
+            File file = new File(Environment.getExternalStorageDirectory() + "/kmf" , "Test.txt");
+            OutputStreamWriter archivo = new OutputStreamWriter(new FileOutputStream(file));
+            String texto = "Hola";
+            archivo.write(encriptar(texto));
+            archivo.flush();
+            archivo.close();
+           }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+    }
+    private String encriptar(String texto)
+    {
+        try {
+
+            //Obtenemos el texto desde el cuadro de texto
+            String original = texto;
+
+            RSA rsa = new RSA();
+
+            //le asignamos el Contexto
+            rsa.setContext(getBaseContext());
+
+            //Generamos un juego de claves
+            rsa.genKeyPair(1024);
+
+            //Guardamos en la memoria las claves
+            rsa.saveToDiskPrivateKey("rsa.pri");
+            rsa.saveToDiskPublicKey("rsa.pub");
+
+            //Ciframos
+            String encode_text = rsa.Encrypt(original);
+
+            //Mostramos el texto cifrado
+
+            //Creamos otro objeto de nuestra clase RSA
+            RSA rsa2 = new RSA();
+
+            //Le pasamos el contexto
+            rsa2.setContext(getBaseContext());
+
+            //Cargamos las claves que creamos anteriormente
+            rsa2.openFromDiskPrivateKey("rsa.pri");
+            rsa2.openFromDiskPublicKey("rsa.pub");
+
+            //Desciframos
+            String decode_text = rsa2.Decrypt(encode_text);
+
+            //Mostramos el texto ya descifrado
+
+            return encode_text;
+        }
+        catch (Exception e) {
+            return "";
+        }
+
+    }
+
 }
